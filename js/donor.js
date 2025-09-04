29d3cbd6a6ca4344d391ad7df6d47f258da86fc5
unknown <jaydencpaul@gmail.com>
2025-09-04 19:25:40 +0800

    Te

--- js/donor.js
+++ js/donor.js
@@ -1020,6 +1020,20 @@ const wishEmotion = document.getElementById('wishEmotion');
 const wishSituation = document.getElementById('wishSituation');
 const wishText = document.getElementById('wishText');
 
+function shadeColor(hex, percent) {
+  let num = parseInt(hex.replace('#',''),16),
+      amt = Math.round(2.55 * percent),
+      R = (num >> 16) + amt,
+      G = (num >> 8 & 0x00FF) + amt,
+      B = (num & 0x0000FF) + amt;
+  return "#" + (
+    0x1000000 +
+    (R<255?R<1?0:R:255)*0x10000 +
+    (G<255?G<1?0:G:255)*0x100 +
+    (B<255?B<1?0:B:255)
+  ).toString(16).slice(1).toUpperCase();
+}
+
 let currentWishId = null;
 
 async function openModal(wishId) {
