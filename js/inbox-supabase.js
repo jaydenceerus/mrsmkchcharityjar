91fb64131fe42f665517a4a1fc480c7507db9e4b
jaydenceerus <jaydencpaul@gmail.com>
2026-06-11 13:49:02 +0800

    Update inbox-supabase.js

--- js/inbox-supabase.js
+++ js/inbox-supabase.js
@@ -9,8 +9,9 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 // ---------- CONFIG ----------
-const SUPABASE_URL = "https://gfkxeplpiojmyibcfbum.supabase.co"; // replace
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdma3hlcGxwaW9qbXlpYmNmYnVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkzNTQ4MzUsImV4cCI6MjA5NDkzMDgzNX0.E_3BrKvhGFrVBnBiJjc6jnxsakiHtYcbntHp1QEEGQ4";                          // replace
+const SUPABASE_URL      = 'https://gybcidzljqrmlhpjlxib.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd5YmNpZHpsanFybWxocGpseGliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODExNDk4MzAsImV4cCI6MjA5NjcyNTgzMH0.MAy1jhw5cdbaed8I0iXnjpdURJGa8wP-kMN43SCcOMI'  // your anon key
+
 // ----------------------------
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
